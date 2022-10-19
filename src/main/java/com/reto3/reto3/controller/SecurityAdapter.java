b6b96ce0a184c1c6539d8124cb28d6b171c4e5a9
wilfredo Abril Diaz <abrildiazwilfredo@gmail.com>
2022-10-19 18:01:04 -0500

    cambio

--- src/main/java/com/reto3/reto3/controller/SecurityAdapter.java
+++ src/main/java/com/reto3/reto3/controller/SecurityAdapter.java
@@ -14,7 +14,7 @@ public class SecurityAdapter extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests(
                 a -> a.antMatchers(                    
-                    "/", "/error", "/webjars/**","/api/**","/**/*.css","/**/*.jpg","/**/*.png"
+                    "/", "/error", "/webjars/**","/index.html/**","/api/**","/**/*.css","/**/*.jpg","/**/*.png"
                 ).permitAll().anyRequest().authenticated()
         ).exceptionHandling(
             e -> e.authenticationEntryPoint( new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
